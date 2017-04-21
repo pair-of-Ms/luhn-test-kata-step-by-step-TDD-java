9cd598f9cbdb97ef698c351d32cd901f18e1a32e
trikitrok <trikitrok@gmail.com>
2017-04-21 21:22:40 +0200

    playing with functions and streams

--- src/main/java/luhn/LuhnTest.java
+++ src/main/java/luhn/LuhnTest.java
@@ -1,20 +1,42 @@
 package luhn;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.IntPredicate;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class LuhnTest {
 
     public static boolean passes(String digits) {
-        String reversedDigits = new StringBuilder(digits).reverse().toString();
+        List<Integer> reversed = reverse(digits).stream().
+            map(i -> Integer.parseInt(i)).
+            collect(Collectors.toList());
 
-        int result = Integer.parseInt(reversedDigits.substring(0,1)) +
-            Integer.parseInt(reversedDigits.substring(2,3)) +
-
-            addDigitComponents((Integer.parseInt(reversedDigits.substring(1, 2)) * 2)) +
-            addDigitComponents((Integer.parseInt(reversedDigits.substring(3,4)) * 2));
+        int result = sum(digitsAtPositionsFulfilling(isEven, reversed)) +
+            sum(digitsAtPositionsFulfilling(isEven.negate(), reversed).map(i -> addDigitComponents(2 * i)));
 
         return (result % 10) == 0;
     }
 
-    private static int addDigitComponents(int digit){
+    private static Integer sum(Stream<Integer> digits) {
+        return digits.reduce(0, (a, b) -> a + b);
+    }
+
+    private static List<String> reverse(String digits) {
+        return Arrays.asList(new StringBuilder(digits).reverse().toString().split(""));
+    }
+
+    private static IntPredicate isEven = (x) -> x % 2 == 0;
+
+    private static Stream<Integer> digitsAtPositionsFulfilling(IntPredicate f, List<Integer> reversed) {
+        return IntStream.range(0, reversed.size()).
+            filter(f).
+            mapToObj(reversed::get);
+    }
+
+    private static int addDigitComponents(int digit) {
         return (digit / 10) + (digit % 10);
     }
 }
